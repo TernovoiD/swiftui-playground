2be28bfd0f533656f0a9f6c3f0759282000e2c99
Danylo Ternovoi <121759315+TernovoiD@users.noreply.github.com>
2023-01-29 19:37:56 +0200

    Initial Commit

---
+++ UIPlayground/UIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPlaygroundApp.swift
+//  UIPlayground
+//
+//  Created by Danylo Ternovoi on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
